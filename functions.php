b7cd2ff2162fb872fdc99c9db92fc89f6362c554
David Stehle <davidjstehle@gmail.com>
2014-07-08 13:19:36 -0400

    code snippet

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+// ---------- HIDE EMAIL ADDRESS FROM SPAMMERS ----------
+function wpcodex_hide_email_shortcode( $atts , $content = null ) {
+	if ( ! is_email( $content ) ) {
+		return;
+	}
+
+	return '<a href="mailto:' . antispambot( $content ) . '">' . antispambot( $content ) . '</a>';
+}
+add_shortcode( 'email', 'wpcodex_hide_email_shortcode' );
